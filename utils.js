3ae809fcb497364585c37287111e4225a86a5bc6
Toam10 <78689759+Toam10@users.noreply.github.com>
2022-01-17 02:34:31 +0200

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+// utils needs to be a pure functions not a complex functions and more then that dont replace it as the controller
 
 const getAllUsers = () => {
   try {
@@ -9,8 +10,11 @@ const getAllUsers = () => {
     return [];
   }
 };
+// can be in one line
+// good name
 
 const getOneUser = (id) => {
+  // const {id} = req.params;
   try {
     const users = getAllUsers();
     const wantedId = users.find((user) => {
@@ -27,24 +31,34 @@ const getOneUser = (id) => {
   }
 };
 
+// please use req,res
+// nice use of getAllUsers
+
 const addNewUsers = (body) => {
   try {
     const users = getAllUsers();
     users.find((user) => {
       if (user.id === body.id) {
         //check if id is exist in our data.
+        // dont give a comments and this check could go to utils like isUserExist function 
         throw Error('The user is allready exist');
       }
     });
     users.push(body);
     const dataJSON = JSON.stringify(users);
+    // you can do it inside the writeFileSync method
     //method converts a JavaScript object or value to a JSON string
-    fs.writeFileSync('./database/users.json', dataJSON);
+    
+    fs.writeFileSync('./database/users.json', dataJSON)
+    // move to utils as a saveUsers function
+    
     return dataJSON;
+    // long function to much 
   } catch (error) {
     return error.message;
   }
 };
+
 
 const deposit = (id, amount) => {
   let bool = false;
@@ -53,44 +67,61 @@ const deposit = (id, amount) => {
     const userMap = users.map((user) => {
       if (user.id === id) {
         bool = true;
-        console.log('hila', user.id);
+        // use that a bool but why ? this name doesn't give me any info about it
+        
+        console.log('hila', user.id); // remove console.log
         return {
           id: user.id,
-          cash: Number(user.cash) + Number(amount),
+          cash: +user.cash + +amount,
           credit: user.credit,
         };
+        // return object that you give him a good name and the return it i dont need to guess what you want to build with this values
       }
       return user;
     });
-    if (!bool) {
-      throw Error('wrong');
-    }
-    console.log('ggg', userMap);
+    if (!bool) throw Error('wrong'); // one line
+    
+    console.log('ggg', userMap); // remove console.log
+    
     const dataJSON = JSON.stringify(userMap);
+    // userMap I dont think that U know what Map means please make the name clearer (Map more for cash some data) (Array.map array method)
+    
     fs.writeFileSync('./database/users.json', dataJSON);
+     // see above
+    
     return dataJSON;
   } catch (error) {
     return error.message;
+    // `Error:${error.message}`
   }
 };
 
 const credit = (id, creditupdate) => {
   const users = getAllUsers();
   try {
-    if (Number(creditupdate) > 0) {
+    if (+creditupdate > 0) {
+      // really big if
       const userMap = users.map((user) => {
         if (user.id === id) {
           return {
             id: user.id,
             cash: user.cash,
-            credit: Number(creditupdate),
+            credit: +creditupdate,
+              // return object that you give him a good name and the return it i dont need to guess what you want to build with this values
           };
         }
+        
         return user;
       });
+      
+      // move to utils
 
       const dataJSON = JSON.stringify(userMap);
+       // userMap I dont think that U know what Map means please make the name clearer (Map more for cash some data) (Array.map array method)
+      
       fs.writeFileSync('./database/users.json', dataJSON);
+      // see above
+      
       return dataJSON;
     } else {
       throw Error('Eric dont like this number');
@@ -118,7 +149,7 @@ const withdraw = (id, money) => {
       }
       return user;
     });
-    // console.log('ggg', userMap);
+    // console.log('ggg', userMap);// remove console.log by the way "ggg" that not something that you want me to see (: maybe "userMap:"
     const dataJSON = JSON.stringify(userMap);
     fs.writeFileSync('./database/users.json', dataJSON);
     return dataJSON;
@@ -136,6 +167,7 @@ const transfer = (id, reciver, money) => {
         user.cash = +user.cash + +money;
         return user;
       }
+  
 
       if (user.id === id) {
         user.id = +id;
@@ -145,6 +177,8 @@ const transfer = (id, reciver, money) => {
 
       return user;
     });
+        // good code but move to utils
+        // and please pick a good name
 
     return newUsers;
   } catch (error) {
@@ -152,7 +186,7 @@ const transfer = (id, reciver, money) => {
   }
 };
 
-//     console.log('findreciver', findreciver);
+//     console.log('findreciver', findreciver); // remove comments this is called zombe code meaningless maybe save some refrence in you PC but not here (:
 //     const findUser = users.map((user) => {
 //       if (user.id === parseInt(id)) {
 //         console.log(user.id);
